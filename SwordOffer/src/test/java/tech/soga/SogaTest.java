d01a487935da96a025d2ac34e209ce1a960337ef
sogatechnology <sogatech@88.com>
2021-02-08 23:55:34 +0800

    problem list

--- SwordOffer/src/test/java/tech/soga/SogaTest.java
+++ SwordOffer/src/test/java/tech/soga/SogaTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package tech.soga;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class SogaTest
 {
     /**
      * Rigorous Test :-)
